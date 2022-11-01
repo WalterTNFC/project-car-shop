b67fc661732738dde4c0c64847fae0b1d5e55909
WalterTNFC <walter.tad@hotmail.com>
2022-10-31 22:44:13 -0300

    feat: controller

---
+++ src/controllers/carsController.ts
@@ -0,0 +1,33 @@
+import { NextFunction, Request, Response } from 'express';
+import { ICar } from '../interfaces/ICar';
+import IService from '../interfaces/IService';
+
+class CarsController {
+  private _carService: IService<ICar>;
+  constructor(service: IService<ICar>) {
+    this._carService = service;
+  }
+
+  public create = async (req: Request, res: Response, next: NextFunction) => {
+    const { model, year, color, status, buyValue, seatsQty, doorsQty } = req.body;
+    try {
+      const { code, data } = await this._carService.create({
+        model, year, color, status, buyValue, seatsQty, doorsQty,
+      });
+      return res.status(code).json(data);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public read = async (_req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { code, data } = await this._carService.read();
+      return res.status(code).json(data);
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+
+export default CarsController;
